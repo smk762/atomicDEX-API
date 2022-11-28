3cf2caab5d1d6fd145bf07566edc5e591caad026
Artem Vitae <email@not.set>
2022-11-28 11:51:04 +0300

    IRIS released 1.4.1 - stop ignoring related tests.

--- mm2src/coins/tendermint/tendermint_coin.rs
+++ mm2src/coins/tendermint/tendermint_coin.rs
@@ -2126,9 +2126,7 @@ pub mod tendermint_coin_tests {
         // >> END HTLC CLAIMING
     }
 
-    /// TODO ignore it for a while.
     #[test]
-    #[ignore]
     fn try_query_claim_htlc_txs_and_get_secret() {
         let rpc_urls = vec![IRIS_TESTNET_RPC_URL.to_string()];
 
@@ -2184,9 +2182,7 @@ pub mod tendermint_coin_tests {
         assert_eq!(actual_secret, expected_secret);
     }
 
-    /// TODO ignore it for a while.
     #[test]
-    #[ignore]
     fn wait_for_tx_spend_test() {
         let rpc_urls = vec![IRIS_TESTNET_RPC_URL.to_string()];
 
@@ -2498,9 +2494,7 @@ pub mod tendermint_coin_tests {
         };
     }
 
-    /// TODO ignore it for a while.
     #[test]
-    #[ignore]
     fn test_search_for_swap_tx_spend_spent() {
         let rpc_urls = vec![IRIS_TESTNET_RPC_URL.to_string()];
 
