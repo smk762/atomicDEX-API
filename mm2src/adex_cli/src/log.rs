117a3f24f06b5ff3dda3187c438de96a6de10f96
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2023-03-24 11:36:04 +0500

    Get rid of log4rs

--- mm2src/adex_cli/src/log.rs
+++ mm2src/adex_cli/src/log.rs
@@ -1,18 +1,10 @@
-use common::log::LevelFilter;
-use log4rs::{append::console::ConsoleAppender,
-             config::{Appender, Root},
-             encode::pattern::PatternEncoder,
-             Config};
-
-const REDUCED_LOG_FORMAT: &str = "{m}{n}";
+use env_logger;
+use log::LevelFilter;
+use std::io::Write;
 
 pub fn init_logging() {
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(REDUCED_LOG_FORMAT)))
-        .build();
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
-        .expect("Failed to build log4rs config");
-    log4rs::init_config(config).expect("Failed to init log4rs config");
+    env_logger::builder()
+        .format(|buf, record| writeln!(buf, "{}", record.args()))
+        .filter_level(LevelFilter::Info)
+        .init();
 }
