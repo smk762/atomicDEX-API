4aac321e6dec4d4fd48ed354661036360c2e6b50
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2023-03-27 21:49:13 +0500

    Provide macos configuration

--- mm2src/adex_cli/src/scenarios/mm2_proc_mng.rs
+++ mm2src/adex_cli/src/scenarios/mm2_proc_mng.rs
@@ -57,6 +57,7 @@ fn get_mm2_binary_path() -> Result<PathBuf, ()> {
     Ok(dir)
 }
 
+#[cfg(all(any(unix, windows), not(macos)))]
 pub fn start_process(mm2_cfg_file: &Option<String>, coins_file: &Option<String>, log_file: &Option<String>) {
     let mm2_binary = match get_mm2_binary_path() {
         Err(_) => return,
@@ -78,7 +79,7 @@ pub fn start_process(mm2_cfg_file: &Option<String>, coins_file: &Option<String>,
     start_process_impl(mm2_binary);
 }
 
-#[cfg(unix)]
+#[cfg(all(unix, not(macos)))]
 pub fn start_process_impl(mm2_binary: PathBuf) {
     let mut command = Command::new(&mm2_binary);
     let program = mm2_binary
@@ -135,7 +136,7 @@ pub fn start_process_impl(mm2_binary: PathBuf) {
     }
 }
 
-#[cfg(unix)]
+#[cfg(all(unix, not(macos)))]
 pub fn stop_process() {
     let pids = find_proc_by_name(MM2_BINARY);
     if pids.is_empty() {
@@ -170,4 +171,14 @@ pub fn stop_process() {
             TerminateProcess(handy, 1);
         }
     }
+}
+
+#[cfg(macos)]
+pub fn start_process(mm2_cfg_file: &Option<String>, coins_file: &Option<String>, log_file: &Option<String>) {
+    unimplemented!();
+}
+
+#[cfg(macos)]
+pub fn stop_process() {
+    unimplemented!();
 }
