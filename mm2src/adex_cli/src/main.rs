52767e2dc2c56d92deb5a6b31034a01100bba3cd
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2023-03-28 01:55:35 +0500

    Try to ignore adex-cli for wasm32

--- mm2src/adex_cli/src/main.rs
+++ mm2src/adex_cli/src/main.rs
@@ -1,7 +1,8 @@
-mod cli;
-mod log;
-mod scenarios;
+#[cfg(not(target_arch = "wasm32"))] mod cli;
+#[cfg(not(target_arch = "wasm32"))] mod log;
+#[cfg(not(target_arch = "wasm32"))] mod scenarios;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     log::init_logging();
     cli::process_cli();
