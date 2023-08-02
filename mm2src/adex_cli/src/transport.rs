867a01ab04a57bab93ed52adf5729d165a43698a
Rozhkov Dmitrii <rozhkovdmitrii@yandex.ru>
2023-08-03 04:19:06 +0500

    feat(adex-cli): add support for https connection (#1910)

--- mm2src/adex_cli/src/transport.rs
+++ mm2src/adex_cli/src/transport.rs
@@ -1,9 +1,11 @@
 use anyhow::{anyhow, bail, Result};
 use async_trait::async_trait;
 use http::{HeaderMap, StatusCode};
-use log::{error, warn};
-use mm2_net::native_http::slurp_post_json;
 use serde::{Deserialize, Serialize};
+
+use common::log::{error, warn};
+use hyper_dangerous::get_hyper_client_dangerous;
+use mm2_net::native_http::SlurpHttpClient;
 
 use crate::{error_anyhow, error_bail, warn_bail};
 
@@ -32,8 +34,10 @@ impl Transport for SlurpTransport {
         OkT: for<'a> Deserialize<'a>,
         ErrT: for<'a> Deserialize<'a>,
     {
-        let data = serde_json::to_string(&req).expect("Failed to serialize enable request");
-        match slurp_post_json(&self.rpc_uri, data).await {
+        let data = serde_json::to_string(&req)
+            .map_err(|error| error_anyhow!("Failed to serialize data being sent: {error}"))?;
+        let client = get_hyper_client_dangerous()?;
+        match client.slurp_post_json(&self.rpc_uri, data).await {
             Err(error) => error_bail!("Failed to send json: {error}"),
             Ok(resp) => resp.process::<OkT, ErrT>(),
         }
@@ -75,6 +79,57 @@ impl Response for (StatusCode, HeaderMap, Vec<u8>) {
             _ => {
                 warn_bail!("Bad http status: {status}, data: {data:?}")
             },
+        }
+    }
+}
+
+mod hyper_dangerous {
+
+    use hyper::{client::HttpConnector, Body, Client};
+    use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+    use rustls::client::{ServerCertVerified, ServerCertVerifier};
+    use rustls::{RootCertStore, DEFAULT_CIPHER_SUITES, DEFAULT_VERSIONS};
+    use std::sync::Arc;
+    use std::time::SystemTime;
+
+    use super::*;
+
+    pub(super) fn get_hyper_client_dangerous() -> Result<Client<HttpsConnector<HttpConnector>>> {
+        let mut config = rustls::ClientConfig::builder()
+            .with_cipher_suites(DEFAULT_CIPHER_SUITES)
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(DEFAULT_VERSIONS)
+            .map_err(|error| error_anyhow!("Inconsistent cipher-suite/versions selected: {error}"))?
+            .with_root_certificates(RootCertStore::empty())
+            .with_no_client_auth();
+
+        config
+            .dangerous()
+            .set_certificate_verifier(Arc::new(NoCertificateVerification {}));
+
+        let https_connector = HttpsConnectorBuilder::default()
+            .with_tls_config(config)
+            .https_or_http()
+            .enable_http1()
+            .enable_http2()
+            .build();
+
+        Ok(Client::builder().build::<_, Body>(https_connector))
+    }
+
+    struct NoCertificateVerification {}
+
+    impl ServerCertVerifier for NoCertificateVerification {
+        fn verify_server_cert(
+            &self,
+            _: &rustls::Certificate,
+            _: &[rustls::Certificate],
+            _: &rustls::ServerName,
+            _: &mut dyn Iterator<Item = &[u8]>,
+            _: &[u8],
+            _: SystemTime,
+        ) -> Result<ServerCertVerified, rustls::Error> {
+            Ok(ServerCertVerified::assertion())
         }
     }
 }
--- mm2src/mm2_net/src/native_http.rs
+++ mm2src/mm2_net/src/native_http.rs
@@ -1,11 +1,211 @@
-use crate::transport::{SlurpError, SlurpResult, SlurpResultJson};
-use common::wio::{drive03, HYPER};
-use common::APPLICATION_JSON;
+//! Facilitates execution of http requests
+//!
+//! # Layout
+//!
+//! This module contains several service functions like [slurp_post_json] or [slurp_url_with_headers]
+//! for executing http protocol requests, allowing you to set certain headers, make a [`Request`] in JSON or [`Body`] format.
+//!
+//! These methods are wrappers over [`HYPER`], which is actually `Client<HttpsConnector<HttpConnector>>` that implements
+//! [`SlurpHttpClient`] trait designed to provide http capabilities through it.
+//!
+//! There are also facilities for constructing [SlurpError] from the [hyper::Error]
+//!
+
+use async_trait::async_trait;
 use futures::channel::oneshot::Canceled;
 use http::{header, HeaderValue, Request};
-use hyper::Body;
-use mm2_err_handle::prelude::*;
+use hyper::client::connect::Connect;
+use hyper::client::ResponseFuture;
+use hyper::{Body, Client};
 use serde_json::Value as Json;
+
+use common::wio::{drive03, HYPER};
+use common::APPLICATION_JSON;
+use mm2_err_handle::prelude::*;
+
+use super::transport::{SlurpError, SlurpResult, SlurpResultJson};
+
+/// Provides requesting http through it
+///
+/// Initially designed to be used with [hyper::Client] that could be constructed in different specific ways.
+/// one of which is using with statically defined [HYPER] that is common client able to request https or https urls
+/// In the other case it can be a dangerous client that does not verify self signed signature
+///
+/// # Examples
+///
+/// Request over both http or https using common [hyper_rustls::HttpsConnectorBuilder]
+///
+/// ```rust
+/// let https = HttpsConnectorBuilder::new()
+///     .with_webpki_roots()
+///     .https_or_http()
+///     .enable_http1()
+///     .enable_http2()
+///     .build();
+/// let client = Client::builder().pool_max_idle_per_host(0).build(https)
+/// client.slurp_url(`https://komodoproject.com`)
+/// ```
+///
+/// Request over https with self-signed certificate
+///
+/// ```rust
+/// let data = serde_json::to_string(&req).map_err(|error| error_anyhow!("Failed to serialize data being sent: {error}"))?;
+/// match HYPER_DANGEROUS.slurp_post_json(&self.rpc_uri, data).await {
+///     Err(error) => error_bail!("Failed to send json: {error}"),
+///     Ok(resp) => resp.process::<OkT, ErrT>(),
+/// }
+///
+/// mod hyper_dangerous {
+///     use hyper::{client::HttpConnector, Body, Client};
+///     use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+///     use lazy_static::lazy_static;
+///     use rustls::client::{ServerCertVerified, ServerCertVerifier};
+///     use rustls::{RootCertStore, DEFAULT_CIPHER_SUITES, DEFAULT_VERSIONS};
+///     use std::sync::Arc;
+///     use std::time::SystemTime;
+///
+///     lazy_static! {
+///         pub(super) static ref HYPER_DANGEROUS: Client<HttpsConnector<HttpConnector>> = get_hyper_client_dangerous();
+///     }
+///
+///     fn get_hyper_client_dangerous() -> Client<HttpsConnector<HttpConnector>> {
+///         let mut config = rustls::ClientConfig::builder()
+///             .with_cipher_suites(&DEFAULT_CIPHER_SUITES)
+///             .with_safe_default_kx_groups()
+///             .with_protocol_versions(&DEFAULT_VERSIONS)
+///             .expect("inconsistent cipher-suite/versions selected")
+///             .with_root_certificates(RootCertStore::empty())
+///             .with_no_client_auth();
+///
+///         config
+///             .dangerous()
+///             .set_certificate_verifier(Arc::new(NoCertificateVerification {}));
+///
+///         let https_connector = HttpsConnectorBuilder::default()
+///             .with_tls_config(config)
+///             .https_or_http()
+///             .enable_http1()
+///             .build();
+///
+///         Client::builder().build::<_, Body>(https_connector)
+///     }
+///
+///     struct NoCertificateVerification {}
+///
+///     impl ServerCertVerifier for NoCertificateVerification {
+///         fn verify_server_cert(
+///             &self,
+///             _: &rustls::Certificate,
+///             _: &[rustls::Certificate],
+///             _: &rustls::ServerName,
+///             _: &mut dyn Iterator<Item = &[u8]>,
+///             _: &[u8],
+///             _: SystemTime,
+///         ) -> Result<ServerCertVerified, rustls::Error> {
+///             Ok(ServerCertVerified::assertion())
+///         }
+///     }
+/// }
+/// ```
+#[async_trait]
+pub trait SlurpHttpClient {
+    /// Provides a [ResponseFuture] that could be spawned and processed asynchronously
+    fn request(&self, req: Request<Body>) -> ResponseFuture;
+
+    /// Executes a POST request, returning the response status, headers and body.
+    async fn slurp_post_json(&self, url: &str, body: String) -> SlurpResult {
+        let request = Request::builder()
+            .method("POST")
+            .uri(url)
+            .header(header::CONTENT_TYPE, APPLICATION_JSON)
+            .body(body.into())?;
+        self.slurp_req(request).await
+    }
+
+    /// Executes a GET request, returning the response status, headers and body.
+    async fn slurp_url(&self, url: &str) -> SlurpResult {
+        let req = Request::builder().uri(url).body(Vec::new())?;
+        self.slurp_req(req).await
+    }
+
+    /// Executes a GET request with additional headers.
+    /// Returning the response status, headers and body.
+    async fn slurp_url_with_headers(&self, url: &str, headers: Vec<(&'static str, &'static str)>) -> SlurpResult {
+        let mut req = Request::builder();
+        let h = req
+            .headers_mut()
+            .or_mm_err(|| SlurpError::Internal("An error occurred when accessing the request headers".to_string()))?;
+
+        for (key, value) in headers {
+            h.insert(key, HeaderValue::from_static(value));
+        }
+
+        let req = req.uri(url).body(Vec::new())?;
+        self.slurp_req(req).await
+    }
+
+    /// Executes a Hyper request, requires [`Request<Body>`] and return the response status, headers and body as Json.
+    async fn slurp_req_body(&self, request: Request<Body>) -> SlurpResultJson {
+        let uri = request.uri().to_string();
+
+        let request_f = self.request(request);
+        let response = drive03(request_f)
+            .await?
+            .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
+        let status = response.status();
+        let headers = response.headers().clone();
+        let body_bytes = hyper::body::to_bytes(response.into_body())
+            .await
+            .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
+        let body_str = String::from_utf8(body_bytes.to_vec()).map_to_mm(|e| SlurpError::Internal(e.to_string()))?;
+        let body: Json = serde_json::from_str(&body_str)?;
+        Ok((status, headers, body))
+    }
+
+    /// Executes a Hyper request, returning the response status, headers and body.
+    async fn slurp_req(&self, request: Request<Vec<u8>>) -> SlurpResult {
+        let uri = request.uri().to_string();
+        let (head, body) = request.into_parts();
+        let request = Request::from_parts(head, Body::from(body));
+        let request_f = self.request(request);
+        let response = drive03(request_f)
+            .await?
+            .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
+        let status = response.status();
+        let headers = response.headers().clone();
+        let body = response.into_body();
+        let output = hyper::body::to_bytes(body)
+            .await
+            .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
+        Ok((status, headers, output.to_vec()))
+    }
+}
+
+#[async_trait]
+impl<C> SlurpHttpClient for Client<C>
+where
+    C: Connect + Clone + Send + Sync + 'static,
+{
+    fn request(&self, req: Request<Body>) -> ResponseFuture { Client::<C>::request(self, req) }
+}
+
+/// Executes a Hyper request, returning the response status, headers and body.
+pub async fn slurp_req(request: Request<Vec<u8>>) -> SlurpResult { HYPER.slurp_req(request).await }
+
+/// Executes a Hyper request, requires [`Request<Body>`] and return the response status, headers and body as Json.
+pub async fn slurp_req_body(request: Request<Body>) -> SlurpResultJson { HYPER.slurp_req_body(request).await }
+
+/// Executes a GET request, returning the response status, headers and body.
+pub async fn slurp_url(url: &str) -> SlurpResult { HYPER.slurp_url(url).await }
+
+/// Executes a GET request with additional headers.
+/// Returning the response status, headers and body.
+pub async fn slurp_url_with_headers(url: &str, headers: Vec<(&'static str, &'static str)>) -> SlurpResult {
+    HYPER.slurp_url_with_headers(url, headers).await
+}
+
+/// Executes a POST request, returning the response status, headers and body.
+pub async fn slurp_post_json(url: &str, body: String) -> SlurpResult { HYPER.slurp_post_json(url, body).await }
 
 impl From<Canceled> for SlurpError {
     fn from(_: Canceled) -> Self { SlurpError::Internal("Spawned Slurp future has been canceled".to_owned()) }
@@ -29,76 +229,6 @@ impl SlurpError {
 /// `http::Error` can appear on an HTTP request [`http::Builder::build`] building.
 impl From<http::Error> for SlurpError {
     fn from(e: http::Error) -> Self { SlurpError::InvalidRequest(e.to_string()) }
-}
-
-/// Executes a Hyper request, returning the response status, headers and body.
-pub async fn slurp_req(request: Request<Vec<u8>>) -> SlurpResult {
-    let uri = request.uri().to_string();
-    let (head, body) = request.into_parts();
-    let request = Request::from_parts(head, Body::from(body));
-
-    let request_f = HYPER.request(request);
-    let response = drive03(request_f)
-        .await?
-        .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
-    let status = response.status();
-    let headers = response.headers().clone();
-    let body = response.into_body();
-    let output = hyper::body::to_bytes(body)
-        .await
-        .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
-    Ok((status, headers, output.to_vec()))
-}
-
-/// Executes a Hyper request, requires [`Request<Body>`] and return the response status, headers and body as Json.
-pub async fn slurp_req_body(request: Request<Body>) -> SlurpResultJson {
-    let uri = request.uri().to_string();
-
-    let request_f = HYPER.request(request);
-    let response = drive03(request_f)
-        .await?
-        .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
-    let status = response.status();
-    let headers = response.headers().clone();
-    // Get the response body bytes.
-    let body_bytes = hyper::body::to_bytes(response.into_body())
-        .await
-        .map_to_mm(|e| SlurpError::from_hyper_error(e, uri.clone()))?;
-    let body_str = String::from_utf8(body_bytes.to_vec()).map_to_mm(|e| SlurpError::Internal(e.to_string()))?;
-    let body: Json = serde_json::from_str(&body_str)?;
-    Ok((status, headers, body))
-}
-
-/// Executes a GET request, returning the response status, headers and body.
-pub async fn slurp_url(url: &str) -> SlurpResult {
-    let req = Request::builder().uri(url).body(Vec::new())?;
-    slurp_req(req).await
-}
-
-/// Executes a GET request with additional headers.
-/// Returning the response status, headers and body.
-pub async fn slurp_url_with_headers(url: &str, headers: Vec<(&'static str, &'static str)>) -> SlurpResult {
-    let mut req = Request::builder();
-    let h = req
-        .headers_mut()
-        .or_mm_err(|| SlurpError::Internal("An error occured while accessing to the request headers.".to_string()))?;
-
-    for (key, value) in headers {
-        h.insert(key, HeaderValue::from_static(value));
-    }
-
-    let req = req.uri(url).body(Vec::new())?;
-    slurp_req(req).await
-}
-
-/// Executes a POST request, returning the response status, headers and body.
-pub async fn slurp_post_json(url: &str, body: String) -> SlurpResult {
-    let request = Request::builder()
-        .method("POST")
-        .uri(url)
-        .header(header::CONTENT_TYPE, APPLICATION_JSON)
-        .body(body.into())?;
-    slurp_req(request).await
 }
 
 #[cfg(test)]
