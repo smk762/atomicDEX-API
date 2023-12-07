040df1da8bb25cf03322de909e2191e7455d347d
Onur Özkan <onurozkan.dev@outlook.com>
2023-12-07 12:57:36 +0300

    chore(network): exclude `168.119.236.249` from the seednode list (#2021)
    `168.119.236.249` will be used to cut the communication on the old main network by giving information about the new main network, it will be run using this branch https://github.com/KomodoPlatform/komodo-defi-framework/tree/patch-for-deprecation
    
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- mm2src/mm2_p2p/src/network.rs
+++ mm2src/mm2_p2p/src/network.rs
@@ -26,10 +26,11 @@ const ALL_DEFAULT_NETID_SEEDNODES: &[(&str, &str)] = &[
         "12D3KooWHBeCnJdzNk51G4mLnao9cDsjuqiMTEo5wMFXrd25bd1F",
         "168.119.236.243",
     ),
-    (
-        "12D3KooWKxavLCJVrQ5Gk1kd9m6cohctGQBmiKPS9XQFoXEoyGmS",
-        "168.119.236.249",
-    ),
+    // TODO: Uncomment this once re-enabled on the main network.
+    // (
+    //     "12D3KooWKxavLCJVrQ5Gk1kd9m6cohctGQBmiKPS9XQFoXEoyGmS",
+    //     "168.119.236.249",
+    // ),
     (
         "12D3KooW9soGyPfX6kcyh3uVXNHq1y2dPmQNt2veKgdLXkBiCVKq",
         "168.119.236.246",
